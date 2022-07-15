26cacd6703db030a8ea951734477399af7a53fa1
Zac <zac@ZacBookPro.WhiteCrest>
2022-07-15 20:39:41 +1000

    Add class to highlight background div on input when focused

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const formInputs = document.querySelectorAll("input");
 console.log(formInputs);
+
+function identifyFocus() {
+  console.log("Identifying focus");
+  let currentInput = event.target.closest("div");
+  console.log(currentInput);
+  currentInput.classList.add("selected");
+}
+
+formInputs.forEach(function (formInput) {
+  console.log("Binding the input");
+  console.log(formInput.type);
+  formInput.addEventListener("click", identifyFocus);
+});
