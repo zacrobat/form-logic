9e69a4b4f8761d3676f868ee5b3499342cfb68ef
Zac <zac@ZacBookPro.WhiteCrest>
2022-07-20 17:15:11 +1000

    Begin client side validation - check validity

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const formInputs = document.querySelectorAll("input, button");
-console.log(formInputs);
+// console.log(formInputs);
 
 function addHighlight() {
   let currentInput = event.target.closest("div");
@@ -19,4 +19,17 @@ formInputs.forEach(function (formInput) {
   formInput.addEventListener("blur", removeHighlight);
 });
 
-// To-do use removeAttribute(disabled) from button when validations are satisfied
+// To-do: button to validate inputs before submit.
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#validation
+
+// email regexp /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}
+// [a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+function checkEmailIsVald() {
+  let emailField = document.querySelector("input[name='user_email']");
+  console.log(emailField.checkValidity()); // works
+  console.log(emailField.validity.valid); // also works
+}
+
+checkEmailIsVald();
